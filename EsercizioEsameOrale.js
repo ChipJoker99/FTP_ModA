6d201d598120a2bb730d84cd2ee9193cbc757b78
ChipJoker99 <82726784+ChipJoker99@users.noreply.github.com>
2021-10-19 15:59:23 +0200

    Update EsercizioEsameOrale.js

--- EsercizioEsameOrale.js
+++ EsercizioEsameOrale.js
@@ -1,4 +1,5 @@
 //calcolare massimo elementi diagonale principale
+
 function massimo_diagonale(A){
   var i;
   var max = -Infinity;
@@ -10,3 +11,26 @@ function massimo_diagonale(A){
 }
 var M = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, -3, 4]];
 console.log(massimo_diagonale(M));
+
+
+//somma di tutti gli elementi che non sono sulla diagonale
+
+function esame(M)
+{
+	var somma = 0;
+
+	for(var i = 0; i < M.length; i++)
+	{
+		for(var j = 0; j < M[i].length; j++)
+		{
+			if(i!=j)
+				somma += M[i][j];
+		}
+	}
+
+	return somma;
+}
+
+var mat = [[1, 1, -1], [-2, 2, 2], [0, 0, 2]];
+console.log(esame(mat));
+
